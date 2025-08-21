067c2f0693df93979156aafa9e1bfc72584a2cf8
alexandre zenas <alexandrezenas11@gmail.com>
2025-08-21 14:49:38 -0300

    feat: home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/home';
 
 function App() {
   return (
     <>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+export function Home() {
+  return (
+    <main className="py-24">
+        <section className="2xl:max-w-[1440px] xl:max-w-[1280px] mx-auto px-4 ">
+            <h1 className="text-3xl font-bold underline">Home Page</h1>
+        </section>
+    </main>
+  );
+}   
