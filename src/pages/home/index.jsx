84f53170cb1bb910b5e58b532dc3b98191473cc8
alexandre zenas <alexandrezenas11@gmail.com>
2025-08-21 14:57:50 -0300

    feat: add comment

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,13 @@
 export function Home() {
   return (
-    <main className="py-24">
-        <section className="2xl:max-w-[1440px] xl:max-w-[1280px] mx-auto px-4 ">
-            <h1 className="text-3xl font-bold underline">Home Page</h1>
+    <main>
+        {/* delete this section */}
+        <section className=" py-24">
+            <div className="2xl:max-w-[1440px] xl:max-w-[1280px] mx-auto px-4">
+                <h1 className="text-3xl font-bold underline">Home Page</h1>                
+            </div>
         </section>
+        {/* delete this section*/}
     </main>
   );
 }   
